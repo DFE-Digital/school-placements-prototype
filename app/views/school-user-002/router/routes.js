8be34a6a8f05114fcda1df0ffbd70a6a5987ba3f
Peter Jobes <peter.jobes@kinandcarta.com>
2024-04-30 17:19:27 +0100

    scenario-002

--- app/views/school-user-002/router/routes.js
+++ app/views/school-user-002/router/routes.js
@@ -12,6 +12,11 @@ router.get("/school-user-002/placement-updated", (req, res) => {
     res.redirect(req.originalUrl.replace("placement-updated","placement-detail"))
 })
 
+router.get("/school-user-002/placement-updated-2", (req, res) => {
+    req.flash('success', 'Placement updated')
+    res.redirect(req.originalUrl.replace("placement-updated-2","placement-detail-2"))
+})
+
 router.get("/school-user-002/mentor-added", (req, res) => {
     req.flash('success', 'Mentor added')
     res.redirect(req.originalUrl.replace("mentor-added","mentors"))
@@ -22,5 +27,6 @@ router.get("/school-user-002/provider-added", (req, res) => {
     res.redirect(req.originalUrl.replace("provider-added","providers"))
 })
 
+// Routes
 
 }
