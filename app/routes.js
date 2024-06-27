28b5ee8b3070ce67ff986077323bf434f0efcfef
Peter Jobes <peter.jobes@kinandcarta.com>
2024-06-27 12:42:24 +0100

    placements preview first pass

--- app/routes.js
+++ app/routes.js
@@ -21,9 +21,10 @@ router.all('*', (req, res, next) => {
 
 require('./views/school-users/v1/router/routes')(router)
 require('./views/school-users/v1-10/router/routes')(router)
-require('./views/school-users/v1-11/router/routes')(router)
-require('./views/school-users/v1-12/router/routes')(router)
-require('./views/school-users/v1-13/router/routes')(router)
+require('./views/school-users/v1-11/router/routes')(router) // Placement window (months)
+require('./views/school-users/v1-12/router/routes')(router) // Placement window (terms)
+require('./views/school-users/v1-13/router/routes')(router) // Placement window (dates)
+require('./views/school-users/v1-14/router/routes')(router) // Placement preview
 
 //// UR Round 3
 require('./views/ur-archive/school-user-001/router/routes')(router)
---
+++ app/views/school-users/v1-14/router/routes.js
@@ -0,0 +1,73 @@
+module.exports = router => {
+
+// Notifications
+
+router.get("/school-users/v1-14/placement-added", (req, res) => {
+    req.flash('success', 'Placement added')
+    if (req.session.data.onboarding == "true") {
+        req.session.data.onboardingPlacement = "true"
+        res.redirect(req.originalUrl.replace("placement-added","onboarding"))
+    }
+    else {
+        res.redirect(req.originalUrl.replace("placement-added","placements"))
+    }
+})
+
+router.get("/school-users/v1-14/placement-mentor-updated", (req, res) => {
+    req.flash('success', 'Mentor updated')
+    res.redirect(req.originalUrl.replace("placement-mentor-updated","placement-detail"))
+})
+
+router.get("/school-users/v1-14/placement-year-group-updated", (req, res) => {
+    req.flash('success', 'Year group updated')
+    res.redirect(req.originalUrl.replace("placement-year-group-updated","placement-detail"))
+})
+
+router.get("/school-users/v1-14/placement-assigned", (req, res) => {
+    req.flash('success', 'Provider assigned')
+    res.redirect(req.originalUrl.replace("placement-assigned","placement-detail"))
+})
+
+router.get("/school-users/v1-14/mentor-added", (req, res) => {
+    req.flash('success', 'Mentor added')
+    if (req.session.data.onboarding == "true") {
+        req.session.data.mentorTRN = 1
+        req.session.data.onboardingMentor = "true"
+        res.redirect(req.originalUrl.replace("mentor-added","onboarding"))
+    }
+    else {
+        req.session.data.mentorTRN = 1
+        res.redirect(req.originalUrl.replace("mentor-added","mentors"))
+    }
+})
+
+router.get("/school-users/v1-14/provider-added", (req, res) => {
+    req.flash('success', 'Partner provider added')
+    if (req.session.data.onboarding == "true") {
+        req.session.data.onboardingProvider = "true"
+        res.redirect(req.originalUrl.replace("provider-added","onboarding"))
+    }
+    else {
+        res.redirect(req.originalUrl.replace("provider-added","providers"))
+    }
+})
+
+router.get("/school-users/v1-14/itt-added", (req, res) => {
+    req.flash('success', 'Placement contact added')
+    if (req.session.data.onboarding == "true") {
+        req.session.data.onboardingITTcontact = "true"
+        res.redirect(req.originalUrl.replace("itt-added","onboarding"))
+    }
+    else {
+        res.redirect(req.originalUrl.replace("itt-added","details"))
+    }
+})
+
+router.get("/school-users/v1-14/user-added", (req, res) => {
+    req.flash('success', 'User added')
+    res.redirect(req.originalUrl.replace("user-added","users"))
+})
+
+// Onboarding routes
+
+}
