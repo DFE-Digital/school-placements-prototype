205bb6eeaa88ca0723d363ca96895c16673bbf37
Peter Jobes <peter.jobes@kinandcarta.com>
2024-07-10 11:49:19 +0100

    assign trainee

--- app/views/provider-users/v1/router/routes.js
+++ app/views/provider-users/v1/router/routes.js
@@ -2,6 +2,11 @@ module.exports = router => {
 
 // Notifications
 
+router.get("/provider-users/v1/trainee-proposed", (req, res) => {
+    req.flash('success', 'Trainee proposed')
+        res.redirect(req.originalUrl.replace("trainee-proposed","placement-detail"))
+})
+
 // Routes
 
 }
