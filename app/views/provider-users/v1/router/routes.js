8ffa489697e8b6beab858c9cbff9f12aaf47f3a9
Peter Jobes <peter.jobes@kinandcarta.com>
2024-07-12 15:11:53 +0100

    trainee-check-copy

--- app/views/provider-users/v1/router/routes.js
+++ app/views/provider-users/v1/router/routes.js
@@ -3,7 +3,7 @@ module.exports = router => {
 // Notifications
 
 router.get("/provider-users/v1/trainee-proposed", (req, res) => {
-    req.flash('success', 'Trainee proposed')
+    req.flash('success', 'Trainee profile has been sent to the school')
         res.redirect(req.originalUrl.replace("trainee-proposed","placement-detail"))
 })
 
