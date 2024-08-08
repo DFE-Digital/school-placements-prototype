d5bc63b455eded05d72468cdae9de2f6bc61f3fe
Peter Jobes <peter.jobes@kinandcarta.com>
2024-08-08 11:33:37 +0100

    placement window work

--- app/views/provider-users/v1-1/router/routes.js
+++ app/views/provider-users/v1-1/router/routes.js
@@ -2,10 +2,6 @@ module.exports = router => {
 
 // Notifications
 
-router.get("/provider-users/v1/trainee-proposed", (req, res) => {
-    req.flash('success', 'Trainee profile has been sent to the school')
-        res.redirect(req.originalUrl.replace("trainee-proposed","placement-detail"))
-})
 
 // Routes
 
