ae8cd1cc441bffe7e0e66022092661da332a8d8b
Peter Jobes <peter.jobes@kinandcarta.com>
2024-08-13 16:23:03 +0100

    provider sweep updates

--- app/views/provider-users/v1-1/router/routes.js
+++ app/views/provider-users/v1-1/router/routes.js
@@ -1,8 +1,16 @@
 module.exports = router => {
 
 // Notifications
+router.get("/provider-users/v1-1/user-added", (req, res) => {
+    req.flash('success', 'User added')
+    res.redirect(req.originalUrl.replace("user-added","users"))
+})
 
+router.get("/school-users/v2/school-added", (req, res) => {
+    req.flash('success', 'Partner school added')
+        res.redirect(req.originalUrl.replace("school-added","schools"))
 
+})
 // Routes
 
 }
