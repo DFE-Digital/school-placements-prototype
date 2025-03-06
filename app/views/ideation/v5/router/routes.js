61fb24b8fbd1c54b278055d25409968521a2fce9
Peter Jobes <peter.jobes@kinandcarta.com>
2025-03-06 14:50:30 +0000

    expression-of-interest-skeleton

---
+++ app/views/ideation/v5/router/routes.js
@@ -0,0 +1,15 @@
+module.exports = router => {
+
+// Routes
+router.get("/ideation/v5/hosting-question", (req, res) => {
+    if (req.session.data.hostingroute == "hosting") {
+        res.redirect(req.originalUrl.replace("hosting-question","green"))
+    }
+    else if (req.session.data.hostingroute == "interested") {
+        res.redirect(req.originalUrl.replace("hosting-question","amber"))
+    }
+    else {
+        res.redirect(req.originalUrl.replace("hosting-question","not-hosting-why"))
+    }
+})
+}
