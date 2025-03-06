7104eb974aa44dd042119e149dfc6996eb1eee5b
Peter Jobes <peter.jobes@kinandcarta.com>
2025-03-06 15:19:22 +0000

    interested route

--- app/views/ideation/v5/router/routes.js
+++ app/views/ideation/v5/router/routes.js
@@ -6,7 +6,7 @@ router.get("/ideation/v5/hosting-question", (req, res) => {
         res.redirect(req.originalUrl.replace("hosting-question","green"))
     }
     else if (req.session.data.hostingroute == "interested") {
-        res.redirect(req.originalUrl.replace("hosting-question","amber"))
+        res.redirect(req.originalUrl.replace("hosting-question","interested-contact"))
     }
     else {
         res.redirect(req.originalUrl.replace("hosting-question","not-hosting-why"))
